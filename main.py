52a939b62d047bf5ef75faf4fb562dcd38144a11
jortle <johnpmanderson@pm.me>
2025-01-25 01:32:57 -0500

    added new maze button. currently broken doesnt reset the canvas

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 from window import Window
-from maze import Maze
 import sys
-import random
 
 
 def main():
-    num_rows = 12
-    num_cols = 16
-    margin = 50
-    screen_x = 800
-    screen_y = 600
-    cell_size_x = (screen_x - 2 * margin) / num_cols
-    cell_size_y = (screen_y - 2 * margin) / num_rows
+    screen_x = 1800
+    screen_y = 900
     win = Window(screen_x, screen_y)
     sys.setrecursionlimit(10000)
-
-    maze = Maze(
-        margin,
-        margin,
-        num_rows,
-        num_cols,
-        cell_size_x,
-        cell_size_y,
-        win,
-        random.randrange(10000),
-    )
-
-    win.maze = maze
 
     print("Maze created")
 
--- window.py
+++ window.py
@@ -1,28 +1,51 @@
 import tkinter as tk
 from tkinter import Tk, BOTH, Canvas, ttk
+from maze import Maze
+import random
 
 
 class Window:
     def __init__(self, width, height):
-        # maze for method access
-        self.maze = None
+        # dimensions
+        self.width = width
+        self.height = height
+
         # whole window root
 
         self.__root = Tk()
         self.__root.title("Maze Solver")
 
         # Canvas for drawing
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas = Canvas(
+            self.__root, bg="white", height=self.height, width=self.width
+        )
         self.__canvas.pack(fill=BOTH, expand=1)
 
         # buttons
         # solve button
-        solve_button = ttk.Button(self.__root, text="Solve Maze", command=self.solve)
-        self.__canvas.create_window(400, 25, anchor=tk.CENTER, window=solve_button)
-        # random seed
+        solve_button = ttk.Button(
+            self.__root, text="Solve Maze", command=self._solve_button
+        )
+        self.__canvas.create_window(
+            self.width / 2, 25, anchor=tk.CENTER, window=solve_button
+        )
+
+        # New Maze button
+
+        new_maze_button = ttk.Button(
+            self.__root, text="New Maze", command=self._new_maze_button
+        )
+        self.__canvas.create_window(
+            self.width / 4, 25, anchor=tk.CENTER, window=new_maze_button
+        )
+
+        # pause/play animation button
+
+        # reset maze button
 
         # entry box for seed
 
+        # functuonal stuff
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -42,7 +65,32 @@ class Window:
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
 
-    def solve(self):
+    def _solve_button(self):
         if self.maze is None:
             return
         self.maze.solve()
+
+    def _new_maze_button(self):
+        self.num_rows = 19
+        self.num_cols = 38
+        self.margin = 50
+        self.cell_size_x = (self.width - 2 * self.margin) / self.num_cols
+        self.cell_size_y = (self.height - 2 * self.margin) / self.num_rows
+        maze = Maze(
+            self.margin,
+            self.margin,
+            self.num_rows,
+            self.num_cols,
+            self.cell_size_x,
+            self.cell_size_y,
+            self,
+            random.randrange(10000),
+        )
+
+        self.maze = maze
+
+    def _pause_button(self):
+        pass
+
+    def _reset_button(self):
+        pass
